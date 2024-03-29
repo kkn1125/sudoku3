165963f5c5469c3a447971f575f3050e230fd1ea
kkn1125 <xpfjrnseks1@naver.com>
2024-03-29 18:55:36 +0900

    🛠️ fix: base 조정

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ command, mode }) => {
   const PORT = Number(process.env.PORT) || 5000;
 
   const MODE = process.env.NODE_ENV || 'production';
-  const BASE = MODE !== 'production' ? '/sudoku3/' : process.env.BASE;
+  const BASE = MODE === 'production' ? '/sudoku3/' : process.env.BASE;
 
   return {
     // vite config
