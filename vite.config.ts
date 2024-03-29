b43a8f7ed096ea691fb815770646593a79811d96
kkn1125 <xpfjrnseks1@naver.com>
2024-03-29 18:49:22 +0900

    🛠️ fix: mobile 버튼 사이즈 조정

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv } from 'vite';
+import dotenv from 'dotenv';
+import path from 'path';
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
+
+  dotenv.config({
+    path: path.join(path.resolve(), '.env'),
+  });
+
+  const env = loadEnv(mode, process.cwd(), '');
 
   const HOST = process.env.HOST;
   const PORT = Number(process.env.PORT) || 5000;
 
-  const BASE = process.env.BASE || "/sudoku3/";
-  const MODE = process.env.NODE_ENV || "production";
+  const BASE = process.env.BASE || '/sudoku3/';
+  const MODE = process.env.NODE_ENV || 'production';
 
   return {
     // vite config
@@ -20,7 +27,7 @@ export default defineConfig(({ command, mode }) => {
       host: HOST,
       port: PORT,
     },
-    base: MODE === "production" ? BASE : "/",
+    base: MODE === 'production' ? BASE : '/',
     build: {
       minify: true,
       cssMinify: true,
